0abd13237519320ecc90f3314c5a7568d179ca4a
brendasutara <sutarabrenda@gmail.com>
2024-04-15 14:42:55 -0300

    Card grid and responsive design

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,42 +1,29 @@
-function Card() {
+import { Product } from "../../Models/Products"
+
+function Card(data: Product) {
     return (
-        <div className="flex-wrap justify-center items-center w-full max-w-sm rounded-lg shadow m-10">
+        <div className="hover:bg-slate-900 cursor-pointer border-slate-800 flex-wrap gap-4 p-2 border justify-center items-center w-full max-w-sm rounded-lg shadow">
             <a href="#">
                 <figure className="relative">
-                    <img className="rounded-lg" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/08d9aa141708781.6258b4f258a52.png" alt="product image" />
+                    <img className="rounded-lg" src={data.images[0]} alt="product image" />
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="absolute right-0 top-0 size-6 m-4 hover:scale-125">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                     </svg>
                 </figure>
-
             </a>
-            <div className="px-5 py-5">
+            <div className="p-4 space-y-2">
                 <a href="#">
-                    <h5 className="text-xl font-semibold tracking-tight  text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                    <h5 className="truncate uppercase text-xl font-semibold tracking-tight  text-white">{data.title}</h5>
                 </a>
-                <div className="flex items-center mt-2.5 mb-5">
-                    <div className="flex items-center space-x-1 rtl:space-x-reverse">
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                        <svg className="w-4 h-4  text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                        </svg>
-                    </div>
-                    <span className="text-xs font-semibold px-2.5 py-0.5 rounded bg-blue-200 text-blue-800 ms-3">5.0</span>
-                </div>
+                <p className="rounded-lg  opacity-60">{data.category.name}</p>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold  text-white">$2.600</span>
-                    <a href="#" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-pink-500 hover:bg-pink-600 focus:ring-blue-800">Add to cart</a>
+                    <span className="text-3xl font-bold  text-white">${data.price}</span>
+                    <a href="#" className="flex gap-x-2 text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 md:px-3 md:py-1.5 lg:px-5 lg:py-2.5 text-center bg-rose-500 hover:bg-rose-600 focus:ring-rose-800">
+                        <p className="hidden lg:block">Add to cart</p>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                    </a>
                 </div>
             </div>
         </div>
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,28 +1,37 @@
+
+import { useEffect, useState } from "react"
 import Card from "../../Components/Card"
 import Categories from "../../Components/Categories"
 import Header from "../../Components/Header"
 import Layout from "../../Components/Layout"
+import { Product } from "../../Models/Products"
 
 
 
 function Home() {
+  const [items, setItems] = useState<Product[]>([])
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=20')
+      .then(response => response.json())
+      .then(data => setItems(data))
+      .catch(error => console.error('Error fetching data:', error))
+
+  }, [])
+
   return (
     <div>
       <Layout>
         <Header />
         <Categories />
-        <h2 className="text-center text-3xl opacity-60 py-6">Todos los productos</h2>
-        <div className="flex justify-center flex-wrap gap-4">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <h2 className="text-center text-3xl opacity-60 py-6">All the products</h2>
+        <div className="flex flex-col md:grid md:grid-cols-3 lg:grid lg:grid-cols-4 mx-auto items-center w-4/5 justify-center gap-4 md:gap-6  lg:gap-10">
+          {
+            items?.map((item) => {
+              return <Card key={item.id} {...item} />
+            })
+          }
         </div>
-
       </Layout>
 
     </div>
